65ed834443abddea81575b5a667becbd36adbcab
Dark <danglinsheng@126.com>
2016-08-05 16:27:52 +0800

    rm

--- m.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main() {
-
-	printf("He:%d\n", 100);
-	printf("aHaelalob");
-
-}
--- t.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main() {
-	int a=10,b;
-
-	b = a*2;
-
-	printf("%d %d\n", a,b);
-	return 0;
-}
